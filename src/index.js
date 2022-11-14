3d395c041dc204136e7b8bb95b8af4f418fa7d0a
elenak762 <elenak762@gmail.com>
2022-11-14 22:48:22 +0300

    redid the table and declension

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Users from './components/users';
 import './index.css';
-
+import 'bootstrap/dist/css/bootstrap.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
